08c16f8b7cc4d7480a2891fa4b04c4677e107bb0
yijialiang <yijialiang@stormorai.com>
2017-10-25 17:07:54 +0800

    接口的数据源 和初始化方法分离

--- Example/ERComponent/ERViewController.m
+++ Example/ERComponent/ERViewController.m
@@ -30,14 +30,14 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    NSArray *imageNameArr = @[@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507700997644&di=3955e38eace469492190cb2f0533795c&imgtype=0&src=http%3A%2F%2Fpic.qiantucdn.com%2F58pic%2F17%2F66%2F84%2F01c58PIC6uk_1024.jpg",@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507700997645&di=0e26f6fff2bddfdbbd68cef8e148bc77&imgtype=0&src=http%3A%2F%2Fpic2.ooopic.com%2F12%2F01%2F38%2F01bOOOPIC1a_1024.jpg",@"http://upload-images.jianshu.io/upload_images/195046-9d5e2db9773e0ed1.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240",@"http://upload-images.jianshu.io/upload_images/195046-6633cefdb7545c01.JPG?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240",@"http://upload-images.jianshu.io/upload_images/4093530-84f3ff6468f9a09d.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240"];
     NSMutableArray *imageArray = [NSMutableArray array];
     for (int i=0; i<5; i++) {
         CircleImageModel *imageModel = [[CircleImageModel alloc]init];
-        if (i==0) {
-               imageModel.ERImgURL = [NSURL URLWithString:@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507700997644&di=3955e38eace469492190cb2f0533795c&imgtype=0&src=http%3A%2F%2Fpic.qiantucdn.com%2F58pic%2F17%2F66%2F84%2F01c58PIC6uk_1024.jpg"];
-        }else{
-            imageModel.ERImgURL = [NSURL URLWithString:@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1507700997645&di=0e26f6fff2bddfdbbd68cef8e148bc77&imgtype=0&src=http%3A%2F%2Fpic2.ooopic.com%2F12%2F01%2F38%2F01bOOOPIC1a_1024.jpg"];
-        }
+    
+        imageModel.ERImgURL = [NSURL URLWithString:imageNameArr[i]];
+        
         
         __weak typeof(imageModel) weakSelf = imageModel;
         imageModel.clickBlock = ^{
@@ -47,10 +47,11 @@
         [imageArray addObject:imageModel];
     }
 
-    ERCirclePicView *picView = [ERCirclePicView initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 200) PicModels:imageArray LoadImageBlock:^(UIImageView *imageView, NSURL *url) {
+    ERCirclePicView *picView = [ERCirclePicView initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 200) LoadImageBlock:^(UIImageView *imageView, NSURL *url) {
         [imageView setURLImageWithURL:url placeHoldImage:nil isCircle:NO];
     }];
-    
+  
+    picView.picModels = imageArray;
     [self.view addSubview:picView];
     
 	// Do any additional setup after loading the view, typically from a nib.
